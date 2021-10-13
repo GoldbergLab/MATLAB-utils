04db42da114d48f626a4dd14a53263725f2cb0bd
Brian Kardon <brian.kardon@gmail.com>
2021-10-13 12:10:13 -0400

    Created function to intermittently display progress in a loop

---
+++ source/displayProgress.m
@@ -0,0 +1,12 @@
+function displayProgress(template, index, total, numUpdates)
+% Show intermittent progress updates
+% template: A sprintf formatting string containing two integer spots, one
+%   for the current job index, one for the total number of jobs, in that 
+%   order.
+% index: The current job index
+% total: The total number of jobs
+% numUpdates: Approximate number of updates to show.
+
+if mod(index, total/numUpdates) == 0
+    fprintf(template, index, total);
+end
