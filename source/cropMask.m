61eb193221d4c8b8282a46098d749fa247588808
Brian Kardon <brian.kardon@gmail.com>
2022-07-26 16:52:25 -0400

    Added pad capability to getMaskLim

--- source/cropMask.m
+++ source/cropMask.m
@@ -35,23 +35,11 @@ end
 
 % Find the minimum and maximum coordinates for the true values along each
 % axis.
-[xlimits, ylimits] = getMaskLim(mask);
+[xlimits, ylimits] = getMaskLim(mask, pad);
 
 if isempty(xlimits) || isempty(ylimits)
     cropped_mask = [];
     return 
-end
-
-if pad ~= 0
-    % Pad limits
-    xlimits = xlimits + [-pad, pad];
-    ylimits = ylimits + [-pad, pad];
-
-    % Ensure limits do not exceed boundaries of mask
-    xlimits(1) = max(1, xlimits(1));
-    ylimits(1) = max(1, ylimits(1));
-    xlimits(2) = min(size(mask, 1), xlimits(2));
-    ylimits(2) = min(size(mask, 2), ylimits(2));
 end
 
 cropped_mask = mask(xlimits(1):xlimits(2), ylimits(1):ylimits(2));
--- source/getMaskLim.m
+++ source/getMaskLim.m
@@ -1,4 +1,4 @@
-function [xlimits, ylimits] = getMaskLim(mask)
+function [xlimits, ylimits] = getMaskLim(mask, pad)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % getMaskLim: Get the smallest bounding box containing all true values
 % usage:  [xlimits, ylimits] = getMaskLim(mask)
@@ -11,8 +11,13 @@ function [xlimits, ylimits] = getMaskLim(mask)
 %       coordinates of true values within the mask.
 %    zlimits is a 1x2 vector containing the minimum and maximum z
 %       coordinates of true values within the mask.
+%    pad is an optional integer indicating how much extra mask to take
+%       around the edge of the true region. If the padding overlaps the 
+%       edge of the mask, then less padding will be produced. Default is no
+%       padding.
 %
-% Get the smallest bounding box containing all true values
+% Get the smallest bounding box containing all true values, with optional
+%   padding.
 %
 % See also: cropMask, plotMask, get3MaskLim
 %
@@ -22,6 +27,11 @@ function [xlimits, ylimits] = getMaskLim(mask)
 % Real_email = regexprep(Email,{'=','*'},{'@','.'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+% Check if default pad value is needed
+if ~exist('pad', 'var') || isempty(pad)
+    pad = 0;
+end
+
 % Generate 3D coordinate index lists based on the flattened 1D indices of 
 % the true values
 [x, y] = ind2sub(size(mask), find(mask > 0));
@@ -30,3 +40,15 @@ function [xlimits, ylimits] = getMaskLim(mask)
 % axis.
 xlimits = [min(x), max(x)];
 ylimits = [min(y), max(y)];
+
+if pad ~= 0
+    % Pad limits
+    xlimits = xlimits + [-pad, pad];
+    ylimits = ylimits + [-pad, pad];
+
+    % Ensure limits do not exceed boundaries of mask
+    xlimits(1) = max(1, xlimits(1));
+    ylimits(1) = max(1, ylimits(1));
+    xlimits(2) = min(size(mask, 1), xlimits(2));
+    ylimits(2) = min(size(mask, 2), ylimits(2));
+end
