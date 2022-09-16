9d2190e6960481dbe05d380b98da61a90c3475c7
Brian Kardon <brian.kardon@gmail.com>
2022-09-16 09:56:03 -0400

    getMaskSurface now handles 3D stacks of 2D masks

--- source/getMaskSurface.m
+++ source/getMaskSurface.m
@@ -5,14 +5,16 @@ function surface_mask = getMaskSurface(mask, kernel)
 %         surface_mask = getMaskSurface(mask, kernel)
 %
 % where,
-%    mask is a 2D logical mask
+%    mask is a 2D logical mask, or a 3D stack of 2D masks
 %    kernel is an optional argument indicating what convolution kernel to
 %       use for counting neighbors. If not supplied, the default kernel
 %       will count any pixel as a neighbor if it lies within a 3x3 square
-%       centered on the pixel in question.
+%       centered on the pixel in question. If a stack of masks is provided,
+%       each mask in the stack will be convolved individually
 %    surface_mask is a 2D logical mask for which the true pixels are the
 %       subset of true pixels in the original mask which are on the surface
-%       of true regions.
+%       of true regions. If a stack of masks is provided, each mask will be
+%       treated separately, and returned 
 %
 % This function creates a new 2D mask for which pixels are true if they are
 %   true in the original mask, and have at least one false neighbor in the
@@ -29,14 +31,30 @@ function surface_mask = getMaskSurface(mask, kernel)
 % Real_email = regexprep(Email,{'=','*'},{'@','.'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+if length(size(mask)) == 3
+    stacked = true;
+else
+    stacked = false;
+end
+
 if ~exist('kernel', 'var') || isempty(kernel)
     % Create a neighbor-counting convolution kernel
-    kernel = true([3, 3]);
-    kernel(2, 2) = 0;
+    if ~stacked
+        kernel = true([3, 3]);
+        kernel(2, 2) = false;
+    else
+        kernel = false([3, 3, 3]);
+        kernel(2, 1:3, 1:3) = true;
+        kernel(2, 2, 2) = false;
+    end
 end
 
 % For speed, crop mask to the region containing true values
-[cropped_mask, xlimits, ylimits] = cropMask(mask);
+if ~stacked
+    [cropped_mask, xlimits, ylimits] = cropMask(mask);
+else
+    [cropped_mask, xlimits, ylimits, zlimits] = crop3Mask(mask);
+end
 
 % Convolve kernel to determine how many true neighbors each voxel has
 n = convn(cropped_mask, kernel, 'same');
@@ -47,4 +65,8 @@ cropped_surface_mask = cropped_mask & (n < sum(kernel, 'all'));
 
 % Reconstitute surface mask into full size of original mask.
 surface_mask = false(size(mask));
-surface_mask(xlimits(1):xlimits(2), ylimits(1):ylimits(2)) = cropped_surface_mask;
+if ~stacked
+    surface_mask(xlimits(1):xlimits(2), ylimits(1):ylimits(2)) = cropped_surface_mask;
+else
+    surface_mask(xlimits(1):xlimits(2), ylimits(1):ylimits(2), zlimits(1):zlimits(2)) = cropped_surface_mask;
+end
