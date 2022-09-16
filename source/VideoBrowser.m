f3907a82745b42e08b554fbd675320c47ed440f7
Brian Kardon <brian.kardon@gmail.com>
2022-09-16 14:19:27 -0400

    VideoBrowser can now handle color images
    Also added auto-play, some keyboard commands, and allows Navigation line plots with multiple colors (instead of scatter plots) with the new "linec" function.

--- source/VideoBrowser.m
+++ source/VideoBrowser.m
@@ -4,10 +4,24 @@ classdef VideoBrowser < handle
     %       A separate axes contains a 1D graph representing some value for
     %       each frame. Moving the mouse over this navigational axes causes
     %       the video frame to update to the corresponding frame number.
-    properties (Access = private)
+    %
+    %   Keyboard commands:
+    %     space =                    play/stop video
+    %     right/left arrow =         increment/decrement frame number by 1
+    %       frame, or if video is playing, increase/decrease playback speed
+    %     shift-right/left arrow =   increment/decrement frame number by 10
+    %       frames
+    %     control-right/left arrow = increment/decrement frame number by 
+    %       100 frames
+    %     control-g =                jump to a specific frame number
+    %
+    properties (Access = public)
         VideoFrame                                          % An image object containing the video frame image
         FrameMarker         matlab.graphics.primitive.Line  % A line on the NavigationAxes marking what frame is displayed
         FrameNumberMarker   matlab.graphics.primitive.Text  % Text on the NavigationAxes indicating what frame number is displayed
+        PlayJob             timer                           % A timer for playing the video
+        PlayIncrement       double = 1                      % Number of frames the play timer will advance by on each call.
+        NumColorChannels    double                          % Number of color channels in the video (1 for grayscale, 3 for color)
     end
     properties
         MainFigure          matlab.ui.Figure            % The main figure window
@@ -20,18 +34,21 @@ classdef VideoBrowser < handle
         NavigationDataFunction = []     % A function handle that takes video data as an argument, and returns navigation data
         NavigationColor = []            % A color specification for the navigation data. See the color argument in the scatter function.
         CurrentFrameNum = 1             % An integer representing the current frame number
+        PlaybackSpeed = 25              % Playback speed in fps
     end
     methods
         function obj = VideoBrowser(VideoData, NavigationDataOrFcn, NavigationColor)
             % Construct a new VideoBrowser object.
             %   VideoData = a N x H x W double or uint8 array, where N =
             %       the number of frames, H and W are the height and width
-            %       of each frame
+            %       of each frame, or a N x H x W x 3 array, for videos
+            %       with color.
             %   NavigationDataOrFcn = either
             %       1. A 1 x N array, to be plotted in the NavigationAxes
-            %       2. A function handle which takes N x H x W VideoData 
-            %           array as an argument and returns a 1 x N array as a
-            %           result, to be plotted in the NavigationAxes
+            %       2. A function handle which takes N x H x W (x 3)
+            %           VideoData array as an argument and returns a 1 x N 
+            %           array as a result, to be plotted in the 
+            %           NavigationAxes
             %       4. A string referring to one of the predefined
             %           functions:
             %           - 'sum' - plot sum of pixel values in each frame
@@ -57,6 +74,15 @@ classdef VideoBrowser < handle
 
             obj.createDisplayArea();
 
+            if length(size(VideoData)) == 4
+                if size(VideoData, 4) ~= 3
+                    error('Incorrect video color dimension size: 4D videos must have the dimension order N x H x W x 3.');
+                end
+                obj.NumColorChannels = 3;
+            else
+                obj.NumColorChannels = 1;
+            end
+            
             obj.VideoData = VideoData;
 
             obj.NavigationColor = NavigationColor;
@@ -65,16 +91,24 @@ classdef VideoBrowser < handle
                     obj.NavigationData = [];
                     obj.NavigationDataFunction = NavigationDataOrFcn;
                 case 'char'
+                    switch obj.NumColorChannels
+                        case 3
+                            sumDims = [2, 3, 4];
+                        case 1
+                            sumDims = [2, 3];
+                        otherwise
+                            error('Wrong number of color channels: %d', obj.NumColorChannels);
+                    end
                     switch NavigationDataOrFcn
                         case 'sum'
                             obj.NavigationData = [];
-                            obj.NavigationDataFunction = @(videoData)sum(videoData, [2, 3]);
+                            obj.NavigationDataFunction = @(videoData)sum(videoData, sumDims);
                         case 'diff'
                             obj.NavigationData = [];
-                            obj.NavigationDataFunction = @(videoData)smooth(sum(diff(videoData, 1), [2, 3]), 10);
+                            obj.NavigationDataFunction = @(videoData)smooth(sum(diff(videoData, 1), sumDims), 10);
                         case 'compactness'
                             obj.NavigationData = [];
-                            obj.NavigationDataFunction = @(videoData)sum(videoData, [2, 3]) ./ sum(getMaskSurface(videoData), [2, 3]);
+                            obj.NavigationDataFunction = @(videoData)sum(videoData, sumDims) ./ sum(getMaskSurface(videoData), sumDims);
                         otherwise
                             error('Unrecognized named navigation data function: %s.', NavigationDataOrFcn);
                     end
@@ -86,13 +120,65 @@ classdef VideoBrowser < handle
             end
             
         end
+        function playVideo(obj)
+            warning('off', 'MATLAB:TIMER:RATEPRECISION');
+            if obj.PlaybackSpeed < 0
+                obj.PlayIncrement = -1;
+            else
+                obj.PlayIncrement  = 1;
+            end
+            delete(obj.PlayJob);
+            obj.PlayJob = timer();
+            obj.PlayJob.TimerFcn = @(~, ~)obj.playFcn();
+            obj.PlayJob.Period = 1 / abs(obj.PlaybackSpeed);
+            obj.PlayJob.ExecutionMode = 'fixedRate';
+            obj.PlayJob.start();
+            warning('on', 'MATLAB:TIMER:RATEPRECISION');
+        end
+        function playFcn(obj)
+            % Display a new frame of the video while in play mode
+            try
+                obj.incrementFrame(obj.PlayIncrement);
+                drawnow;
+            catch me
+                if strcmp(me.identifier, 'images:imshow:invalidAxes')
+                    % Axes are missing, we're probably shutting down.
+                    % Stop and delete timer.
+                    stop(obj.PlayJob);
+                    delete(obj.PlayJob);
+                else
+                    throw(me);
+                end
+            end
+        end
+        function stopVideo(obj)
+            stop(obj.PlayJob);
+            delete(obj.PlayJob);
+        end
+        function playing = isPlaying(obj)
+            playing = ~isempty(obj.PlayJob) && isvalid(obj.PlayJob) && strcmp(obj.PlayJob.Running, 'on');
+        end
+        function incrementFrame(obj, delta)
+            obj.CurrentFrameNum = obj.CurrentFrameNum + delta;
+        end
+        function deleteDisplayArea(obj)
+            delete(obj.VideoAxes);
+            delete(obj.NavigationAxes);
+            delete(obj.MainFigure);
+        end
+        function regenerateGraphics(obj)
+            % Recreate graphics in case it gets closed
+            obj.deleteDisplayArea();
+            obj.createDisplayArea();
+            obj.drawNavigationData();
+            obj.updateFrameMarker();
+            obj.updateVideoFrame();
+        end
         function createDisplayArea(obj)
             % Create & prepare the graphics containers (the figure & axes)
             
             % Delete graphics containers in case they already exist
-            delete(obj.MainFigure)
-            delete(obj.VideoAxes)
-            delete(obj.NavigationAxes)
+            obj.deleteDisplayArea();
             
             % Create graphics containers
             obj.MainFigure =     figure('Units', 'normalized');
@@ -114,6 +200,8 @@ classdef VideoBrowser < handle
             % Configure callbacks
             obj.MainFigure.WindowButtonMotionFcn = @obj.MouseMotionHandler;
             obj.MainFigure.BusyAction = 'cancel';
+            obj.MainFigure.KeyPressFcn = @obj.KeyPressHandler;
+            obj.MainFigure.SizeChangedFcn = @obj.ResizeHandler;
         end
         function updateVideoFrame(obj)
             % Update the displayed video frame based on the current
@@ -132,6 +220,24 @@ classdef VideoBrowser < handle
             % Clear the NavigtationAxes
             
             cla(obj.NavigationAxes);
+        end
+        function color = getNavigationColorPoint(obj, frameNum)
+            if ischar(obj.NavigationColor)
+                % Single color string provided
+                color = obj.NavigationColor;
+            elseif isnumeric(obj.NavigationColor)
+                if isrow(obj.NavigationColor) && length(obj.NavigationColor) == 3
+                    % Single RGB triplet provided
+                    color = obj.NavigationColor;
+                elseif size(obj.NavigationColor, 2) == 3 && size(obj.NavigationColor, 1) > 1
+                    % 3-column array of RGB triplets provided, one color
+                    % per row
+                    color = obj.NavigationColor(frameNum, :);
+                elseif isvector(obj.NavigationColor)
+                    % Color palette index has been provided
+                    color = obj.NavigationAxes.Colormap(frameNum, :);
+                end
+            end
         end
         function drawNavigationData(obj)
             % Draw the NavigationData on the NavigationAxes. If only a
@@ -155,24 +261,40 @@ classdef VideoBrowser < handle
             else
                 navigationData = obj.NavigationData;
             end
+            obj.clearNavigationData();
             
-            scatter(1:length(navigationData), navigationData, 1, obj.NavigationColor, '.', 'Parent', obj.NavigationAxes);
+            obj.NavigationAxes
+            p = plot([1, length(navigationData)], obj.NavigationAxes.YLim);
+            delete(p);
+            linec(1:length(navigationData), navigationData, 'Color', obj.NavigationColor, 'Parent', obj.NavigationAxes);
+%             scatter(1:length(navigationData), navigationData, 1, obj.NavigationColor, '.', 'Parent', obj.NavigationAxes);
             obj.NavigationAxes.XLim = [1, length(navigationData)];
         end
         function frameData = getCurrentVideoFrameData(obj)
             % Extract the current frame's video data
-            
-            frameData = squeeze(obj.VideoData(obj.CurrentFrameNum, :, :));
+            switch obj.NumColorChannels
+                case 3
+                    frameData = squeeze(obj.VideoData(obj.CurrentFrameNum, :, :, :));
+                case 1
+                    frameData = squeeze(obj.VideoData(obj.CurrentFrameNum, :, :));
+                otherwise
+                    error('Wrong number of color channels: %d', obj.NumColorChannels);
+            end
         end
         function numFrames = getNumFrames(obj)
             % Determine the number of frames in the current VideoData
             
             numFrames = size(obj.VideoData, 1);
         end
-        function updateFrameMarker(obj)
+        function updateFrameMarker(obj, varargin)
             % Update the FrameMarker and FrameNumberMarker on the
             %   NavigationAxes to reflect the CurrentFrameNumber
-            
+            if nargin == 2
+                redraw = varargin{1};
+                if redraw
+                    delete(obj.FrameMarker);
+                end
+            end
             x = obj.CurrentFrameNum;
             if isempty(obj.FrameMarker) || ~isvalid(obj.FrameMarker)
                 obj.FrameMarker = line([x, x], obj.NavigationAxes.YLim, 'Parent', obj.NavigationAxes);
@@ -217,6 +339,20 @@ classdef VideoBrowser < handle
             obj.updateVideoFrame();
             obj.updateFrameMarker();
         end
+        function set.PlaybackSpeed(obj, fps)
+            if abs(fps) > 1000
+                fps = 1000 * sign(fps);
+            end
+            if abs(fps) <= 1
+                fps = 1 * sign(fps);
+            end
+            obj.PlaybackSpeed = fps;
+            % If timer is already running, restart it
+            if obj.isPlaying()
+                obj.stopVideo();
+                obj.playVideo();
+            end
+        end
         function inside = inNavigationAxes(obj, x, y)
             % Determine if the given figure coordinates fall within the
             %   borders of the NavigationAxes or not.
@@ -248,6 +384,61 @@ classdef VideoBrowser < handle
                 frameNum = obj.mapFigureXToFrameNum(x);
                 obj.CurrentFrameNum = frameNum;
             end
+        end
+        function KeyPressHandler(obj, src, event)
+            switch event.Key
+                case 'space'
+                    if obj.isPlaying()
+                        obj.stopVideo();
+                    else
+                        obj.playVideo();
+                    end
+                case 'rightarrow'
+                    if obj.isPlaying()
+                        obj.PlaybackSpeed = obj.PlaybackSpeed + 10;
+                    else
+                        if any(strcmp(event.Modifier, 'shift'))
+                            delta = 10;
+                        elseif any(strcmp(event.Modifier, 'control'))
+                            delta = 100;
+                        else
+                            delta = 1;
+                        end
+                        obj.incrementFrame(delta);
+                    end
+                case 'leftarrow'
+                    if obj.isPlaying()
+                        obj.PlaybackSpeed = obj.PlaybackSpeed - 10;
+                    else
+                        if any(strcmp(event.Modifier, 'shift'))
+                            delta = 10;
+                        elseif any(strcmp(event.Modifier, 'control'))
+                            delta = 100;
+                        else
+                            delta = 1;
+                        end
+                        obj.incrementFrame(-delta);
+                    end
+                case 'g'
+                    if any(strcmp(event.Modifier, 'control'))
+                        % control-g pressed
+                        frame = inputdlg('Enter frame number:', 'Goto frame number');
+                        if ~isempty(frame)
+                            obj.CurrentFrameNum = str2double(frame);
+                        end
+                    end
+            end
+        end
+        function ResizeHandler(obj, ~, ~)
+            obj.updateVideoFrame();
+            obj.updateFrameMarker(true);
+        end
+        function delete(obj)
+            if isvalid(obj.PlayJob)
+                stop(obj.PlayJob);
+            end
+            delete(obj.PlayJob);
+            obj.deleteDisplayArea();
         end
     end
 end
---
+++ source/linec.m
@@ -0,0 +1,122 @@
+function lines = linec(x, y, varargin)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% linec: Draw a primitive line object with multiple colors
+% usage:  lines = linec(x, y, ...)
+%         lines = linec(x, y, 'Color', colors, ...)
+%
+% where,
+%    x is a list of x coordinates for the line segments
+%    y is a list of y coordinates for the line segments
+%    colors is a color specification that can be either:
+%       a) A 1x3 RGB triplet
+%       b) A hexadecimal color code
+%       c) A named color
+%       d) A N x 3 RGB array, where N == length(x) == length(y)
+%       e) A 1 x N array of color palette indices
+%    lines is an array of handles to primitive line objects. Each line
+%       object is a single color section of the overall line.
+%    ... you may also use any other Name/Value pairs specified by the line
+%       function documentation
+%
+% linec behaves almost exactly the same as the line function, but it allows
+%   a different set of 'Color' parameter types. In addition to the normal
+%   types, it also allows:
+%   - Nx3 array of RGB color values, where N == length(x) == length(y)
+%   - 1xN array of color palette indices, where N == length(x) == length(y)
+%   One other difference is that the linec function currently only handles
+%       2D lines.
+%
+% Note that this function can be fairly slow for a large number of 
+%   different colors (> 100). For small numbers of colors, it should be
+%   only slightly slowe than the line function.
+%
+% See also: line
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+parentIdx = find(strcmp(varargin, 'Parent'));
+if isempty(parentIdx)
+    parent = gca();
+else
+    parent = varargin{parentIdx + 1};
+    varargin(parentIdx:parentIdx+1) = [];
+end
+
+colorIdx = find(strcmp(varargin, 'Color'));
+if isempty(colorIdx)
+    colors = {[]};
+    xs = {x};
+    ys = {y};
+else
+    color = varargin{colorIdx + 1};
+    varargin(colorIdx:colorIdx+1) = [];
+
+    if ischar(color)
+        % Single color string provided
+        colors = {color};
+        xs = {x};
+        ys = {y};
+    elseif isnumeric(color)
+        if isrow(color) && length(color) == 3
+            % Single RGB triplet provided
+            colors = color;
+            xs = {x};
+            ys = {y};
+        elseif size(color, 2) == 3 && size(color, 1) > 1
+            % 3-column array of RGB triplets provided, one color
+            % per row
+            idx = 1;
+            colors = {};
+            xs = {};
+            ys = {};
+            while idx <= size(color, 1)
+                matchColor = color(idx, :);
+                matches = ismember(color(idx:end, :), matchColor, 'rows');
+                firstOffset = find(~matches, 1);
+                if isempty(firstOffset)
+                    % No offsets, so this color goes all the way to the end
+                    firstOffset = length(matches) + 1;
+                end
+                colors{end+1} = matchColor;
+                xs{end+1} = x(idx:idx+firstOffset-2);
+                ys{end+1} = y(idx:idx+firstOffset-2);
+                idx = firstOffset;
+            end
+        elseif isvector(color)
+            % Color palette index has been provided
+            idx = 1;
+            colors = {};
+            xs = {};
+            ys = {};
+            while idx <= length(color)
+                matchColor = color(idx);
+                matches = color(idx:end) == matchColor;
+                firstOffset = find(~matches, 1);
+                if isempty(firstOffset)
+                    % No offsets, so this color goes all the way to the end
+                    firstOffset = length(matches) + 1;
+                end
+                colors{end+1} = matchColor;
+                xs{end+1} = x(idx:min(idx+firstOffset-1, length(x)));
+                ys{end+1} = y(idx:min(idx+firstOffset-1, length(x)));
+                idx = idx + firstOffset - 1;
+            end
+            colors = cellfun(@(c)parent.Colormap(c, :), colors, 'UniformOutput', false);
+        else
+            error('Color argument must be a color name, RGB triplet, RGB Nx3 array, or 1xN array of palette indices.');
+        end
+    end
+end
+
+lines = [];
+for segmentNumber = 1:length(colors)
+    if isempty(colors{segmentNumber})
+        lines(end+1) = line(xs{segmentNumber}, ys{segmentNumber}, 'Parent', parent, varargin{:});
+    else
+        lines(end+1) = line(xs{segmentNumber}, ys{segmentNumber}, 'Color', colors{segmentNumber}, 'Parent', parent, varargin{:});
+    end
+end
