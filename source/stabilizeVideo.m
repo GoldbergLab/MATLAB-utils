5655fc33201bbc1bd30e73eb33bbfa897d333843
Brian Kardon <brian.kardon@gmail.com>
2022-10-15 15:42:18 -0400

    Added documentation

--- source/stabilizeVideo.m
+++ source/stabilizeVideo.m
@@ -1,4 +1,37 @@
 function stabilizedVideoData = stabilizeVideo(videoData, stationaryX, stationaryY, crop)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% stabilizeVideo: Stabilize a video with an array of stationary coordinates
+% usage:  stabilizedVideoData = stabilizeVideo(videoData, stationaryX, stationaryY, crop)
+%
+% where,
+%    videoData is a 3D (H x W x N) or 4D (H x W x 3 x N) array representing
+%       video data
+%    stationaryX is a 1D (1 x N) array of x coordinates for a stationary
+%       marker
+%    stationaryY is a 1D (1 x N) array of y coordinates for a stationary
+%       marker
+%    crop is an optional boolean flag indicating whether or not to crop the
+%       video after stabilizing. If false, there will be black regions
+%       around the edge of the frames. If true, there will be no black
+%       regions, but the output frame size will be smaller than the 
+%       original. Default is true.
+%
+% Motion stabilize a video. By providing the x and y coordinates of a
+%   marker in a video, the video's frame locations can be adjusted so the
+%   marker position does not change throughout the video.
+%
+% See also: loadVideoData
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Check if we need default crop value
+if ~exist('crop', 'var') || isempty(crop)
+    crop = true;
+end
 
 % Check validity of inputs
 assert(any(ndims(videoData) == [3, 4]), 'Video data must be a 3D (H x W x N) or 4D (H x W x C x N) array.');
@@ -20,8 +53,6 @@ xMin = min(stationaryX);
 xMax = max(stationaryX);
 yMin = min(stationaryY);
 yMax = max(stationaryY);
-xRange = xMax - xMin;
-yRange = yMax - yMin;
 xMid = round((xMin + xMax)/2);
 yMid = round((yMin + yMax)/2);
 
@@ -30,14 +61,12 @@ videoSize = size(videoData);
 % Generate blank video data to hold output
 stabilizedVideoData = zeros(videoSize, class(videoData));
 
-h = size(videoData, 1);
-w = size(videoData, 2);
-
 x1s = zeros([1, numFrames]);
 x2s = zeros([1, numFrames]);
 y1s = zeros([1, numFrames]);
 y2s = zeros([1, numFrames]);
 
+% Loop over each frame and offset its position to stabilize the marker.
 for frameNum = 1:numFrames
     dx = xMid - stationaryX(frameNum);
     dy = yMid - stationaryY(frameNum);
@@ -65,6 +94,9 @@ if crop
 end
 
 function [shiftedFrame, x1, y1, x2, y2] = shiftFrame(frame, dx, dy)
+% Shift a single frame by the requested amount, and return it, along with
+%   the upper left and lower right coordinates of the resultant shifted 
+%   frame.
 [h, w] = size(frame);
 x1 = max(1, 1 + dx);
 y1 = max(1, 1 + dy);
