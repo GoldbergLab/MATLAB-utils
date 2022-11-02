a9e6a735504ebef9850449a9b95e648f46272851
Brian Kardon <brian.kardon@gmail.com>
2022-11-01 20:50:38 -0400

    Updated initStruct to allow for struct size input

--- source/initStruct.m
+++ source/initStruct.m
@@ -1,4 +1,43 @@
-function newStruct = initStruct(fieldNames)
-c = cell(length(fieldNames),1);
-newStruct = cell2struct(c,fieldNames);
-newStruct = newStruct([]);
+function outputStruct = initStruct(fieldNames, structSize)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% initStruct: Initialize a struct with given fields and size
+% usage:  outputStruct = initStruct(fields, structSize)
+%
+% where,
+%    fieldNames is a 1xN cell array of char arrays representing the names 
+%       of fields the outputStruct will contain
+%    structSize is an optional 1xM array of dimension sizes indicating the
+%       desired shape of the outputStruct. Default is 0, which produces a
+%       0x0 struct array.
+%    outputStruct is a struct array of the desired structSize, with the
+%       requested field names.
+%
+% This fills a notable gap in MATLAB builtin functions regarding struct
+%   arrays - it provides a simple way to initialize struct arrays.
+%
+% See also: 
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% If desired size is not provided, produce a size 0 struct array
+if ~exist('structSize', 'var') || isempty(structSize)
+    structSize = 0;
+end
+
+% Create a cell array of the desired size
+placeholder = cell(structSize);
+% Produce an array of values to interleave with the field names for the 
+%   struct function 
+values = cell(size(fieldNames));
+% Set the first value to the cell array of the desired shape, which will
+%   force the struct function to use that to determine the struct array 
+%   shape
+values{1} = placeholder;
+% Interleave field names and values
+pairs = [fieldNames; values];
+% Construct output struct array
+outputStruct = struct(pairs{:});
