2557c2d514b52d3d456c671fa590e4fdaeba7125
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-06-15 21:59:22 -0400

    Bugfix in token extraction
    Also added documentation, improved argument handling style

--- source/findFilesByRegex.m
+++ source/findFilesByRegex.m
@@ -1,7 +1,8 @@
-function [filepaths, varargout] = findFilesByRegex(rootDir, regex, varargin)
+function [filePaths, varargout] = findFilesByRegex(rootDir, regex, matchPath, recurse, includeFolders, includeFiles)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % findFilesByRegex: Search rootDir for file paths that match the regex
-% usage:  filepaths = findFilesByRegex(rootDir, regex)
+% usage:  filePaths = findFilesByRegex(rootDir, regex)
+%         [filePaths, token1, token2...] = findFilesByRegex(rootDir, regex)
 %
 % where,
 %    rootDir is a char array representing a directory to recursively search
@@ -17,10 +18,34 @@ function [filepaths, varargout] = findFilesByRegex(rootDir, regex, varargin)
 %       Default is false.
 %   includeFiles is an optional boolean flag indicating whether to include 
 %       regular files in results, default true.
+%   filePaths is a cell array of file paths that matched the regex
+%   token1, token2, ... is one or more cell arrays containing the tokens
+%       matched by capturing groups in the regex for each file path.
 %
 % This is a function that returns a simple cell array of paths when given a
 %   root directory and a regular expression to filter the files and folders
 %   within.
+%
+% This function can also return arrays of capturing group tokens found by 
+%   the regex. For example, in a directory containing the files
+%
+%     test1.txt
+%     test4.txt
+%     test7.txt
+%
+%   running the command
+%
+%     [filePaths, fileNums] = findFilesByRegex(rootDir, 'test([0-9])\.txt')
+%
+%   Would produce the following arrays:
+%
+%     filePaths =
+%       {'C:\Users\Username\...\test1.txt',
+%        'C:\Users\Username\...\test4.txt',
+%        'C:\Users\Username\...\test7.txt'}
+%
+%     fileNms = 
+%       {'1', '4', '7'}
 %
 % See also: dir
 %
@@ -30,24 +55,19 @@ function [filepaths, varargout] = findFilesByRegex(rootDir, regex, varargin)
 % Real_email = regexprep(Email,{'=','*'},{'@','.'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-if nargin>2
-    matchPath = varargin{1};
-else
+if ~exist('matchPath', 'var') || isempty(matchPath)
     matchPath = false;
 end
-if nargin>3
-    recurse = varargin{2};
-else
+
+if ~exist('recurse', 'var') || isempty(recurse)
     recurse = true;
 end
-if nargin>4
-    includeFolders = varargin{3};
-else
+
+if ~exist('includeFolders', 'var') || isempty(includeFolders)
     includeFolders = false;
 end
-if nargin>5
-    includeFiles = varargin{4};
-else
+
+if ~exist('includeFiles', 'var') || isempty(includeFiles)
     includeFiles = true;
 end
 
@@ -73,8 +93,14 @@ if ~includeFiles
     files = files([files.isdir]);
 end
 
-filepaths = {};
-varargout = {};
+numTokens = nargout-1;
+
+filePaths = {};
+fileTokens = cell(1, numTokens);
+for k = 1:numTokens
+    fileTokens{k} = {};
+end
+
 for k = 1:length(files)
     [~, name, ext] = fileparts(files(k).name);
     if matchPath
@@ -86,32 +112,49 @@ for k = 1:length(files)
     [match, tokens] = regexp(matchName, regex, 'start', 'tokens');
 
     if match
-        filepaths(end+1) = {fullfile(rootDir, files(k).name)};
-        if ~isempty(tokens) && nargout > 1
+        filePaths(end+1) = {fullfile(rootDir, files(k).name)};
+        if ~isempty(tokens) && numTokens > 0
             tokens = tokens{1};
-            if isempty(varargout)
-                varargout = cell(1, length(tokens));
-                for j = 1:length(tokens)
-                    varargout{j} = {};
-                end
-            end
             for j = 1:length(tokens)
-                varargout{j}{end+1} = tokens{j};
+                fileTokens{j}{end+1} = tokens{j};
             end
         end
     end
 end
 
 if recurse
+    % Recurse over subdirectories
     if ~islogical(recurse) 
-        % recurse is an integer recursion depth, decrement it!
+        % Recurse is an integer recursion depth, decrement it!
         recurse = recurse - 1;
     end
+    % Loop over subdirectories
     for k = 1:length(dirs)
+        % Make sure we're not operating on a dot pseudo directory
         if ~any(strcmp(dirs(k).name, {'.', '..'}))
             dirpath = fullfile(rootDir, dirs(k).name);
-            filepaths = [filepaths, findFilesByRegex(dirpath, regex, matchPath, recurse, includeFolders, includeFiles)];
+            % Initialize file token cell array for subfolder
+            newFileTokens = cell(1, numTokens);
+            % Run function recursively to capture output from subfolder 
+            %   tree
+            [newFilePaths, newFileTokens{:}] = findFilesByRegex(dirpath, regex, matchPath, recurse, includeFolders, includeFiles);
+            % Append subfolder tree path outputs to current folder path 
+            %   outputs
+            filePaths = [filePaths, newFilePaths];
+            % Initialize fileTokens if it doesn't have any tokens from the
+            %   current folder already.
+            if isempty(fileTokens)
+                fileTokens = cell(1, numTokens);
+                for j = 1:numTokens
+                    fileTokens{j} = {};
+                end
+            end
+            % Append subfolder tree token outputs to current folder token outputs
+            for j = 1:numTokens
+                fileTokens{j} = [fileTokens{j}, newFileTokens{j}];
+            end
         end
     end
 end
 
+varargout = fileTokens;
