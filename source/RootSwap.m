6717be717326d35a24cad9975e15bbc97ef093e4
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-07-21 15:11:38 -0400

    Add documentation

--- source/RootSwap.m
+++ source/RootSwap.m
@@ -1,4 +1,25 @@
 function alteredPaths = RootSwap(originalPaths)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% RootSwap: GUI for swapping one drive letter for another in multiple paths
+% usage:  alteredPaths = RootSwap(originalPaths)
+%
+% where,
+%    originalPaths is either a char array representing a file path, or a
+%       cell array of them.
+%    alteredPaths is a cell array containing the original paths with the
+%       root drive letter swapped according to the user's GUI selections
+%
+% This is a wrapper for PathSwap that automatically fills in the
+%   search/replace fields with the root directory of each path.
+%
+% See also: PathSwap
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 % This is a wrapper for PathSwap that automatically fills in the
 % search/replace fields with the root directory of each path.
 
