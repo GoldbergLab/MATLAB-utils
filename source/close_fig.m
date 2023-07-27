d99aa361d27c84a1764fb9dbb28a038ab29862f1
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-07-26 22:17:20 -0400

    Improved close_fig behavior
    Now it can also kill unkillable figures that have messed up CloseRequestFunctions

--- source/close_fig.m
+++ source/close_fig.m
@@ -1,3 +1,15 @@
 function close_fig()
-% Close all figures, even uifigures
-close(findall(0, 'type', 'figure'))
+% Close all figures, even uifigures, even if someone messed with the
+% CloseRequestFcn
+
+try
+    close(findall(0, 'type', 'figure'));
+end
+
+remainingFigs = findall(0, 'type', 'figure');
+for k = 1:length(remainingFigs)
+    try
+        remainingFigs(k).CloseRequestFcn = 'closereq';
+        close(remainingFigs(k));
+    end
+end
