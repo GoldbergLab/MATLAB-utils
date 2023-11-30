98ad6e1e9de73136bb2d19fd577dc7d0b3887399
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2023-11-30 14:32:12 -0500

    Improvements and bugfixes to MemoryCache

--- source/MemoryCache.m
+++ source/MemoryCache.m
@@ -5,80 +5,89 @@ classdef MemoryCache < handle
     end
     properties (Access = private)
         Cache           cell = {}
+        Cached          logical = logical.empty()
         ElementBytes    double = []
         AccessLog       double = []
-        NullElement     Null
         AccessCount     double = 1
     end
     methods
         function obj = MemoryCache(maxElements, maxBytes)
-            obj.NullElement = Null();
-
             if ~exist('maxElements', 'var') || isempty(maxElements)
-                obj.MaxElements = NaN;
+                obj.MaxElements = [];
             else
                 obj.MaxElements = maxElements;
             end
             if ~exist('maxBytes', 'var') || isempty(maxBytes)
-                obj.MaxBytes = NaN;
+                obj.MaxBytes = [];
             else
                 obj.MaxBytes = maxBytes;
             end
         end
-        function isCached = isCached(obj, index)
-            isCached = (obj.Cache{index} ~= obj.NullElement);
-        end
-        function areCached = areCached(obj, indices)
-            areCached = cellfun(@(x)x~=obj.NullElement, obj.Cache(indices), 'UniformOutput', true);
+        function isCached = isCached(obj, indices)
+            % Check if one or more elements are cached
+            lastCachedIndex = size(obj.Cache, 2);
+            inRange = indices <= lastCachedIndex;
+            isCached = false(size(indices));
+            isCached(inRange) = obj.Cached(indices(inRange));
         end
         function accessCount = get.AccessCount(obj)
             accessCount = obj.AccessCount;
             obj.AccessCount = accessCount + 1;
         end
         function removeFromCache(obj, indices)
-            obj.Cache(indices) = {obj.NullElement};
-            for k = size(obj.Cache, 2):-1:1
-                if obj.Cache{k} ~= obj.NullElement
-                    break;
-                end
-            end
-            lastCachedIndex = k;
-            obj.Cache(lastCachedIndex+1:end) = [];
-            obj.ElementBytes(lastCachedIndex+1:end) = [];
-            obj.AccessLog(lastCachedIndex+1:end) = [];
+            lastCachedIndex = size(obj.Cache, 2);
+            
+            indices(indices>lastCachedIndex) = [];
+
+            obj.Cached(indices) = false;
+            obj.Cache(indices) = {[]};
+            obj.ElementBytes(indices) = 0;
+            obj.AccessLog(indices) = 0;
+
+            obj.trimCache();
         end
         function [element, isCached] = retrieveElement(obj, index)
-            element = obj.Cache{index};
             isCached = obj.isCached(index);
-            obj.AccessLog(index) = obj.AccessCount;
+            if ~isCached
+                element = [];
+            else
+                element = obj.Cache{index};
+                obj.AccessLog(index) = obj.AccessCount;
+            end
         end
         function [elements, areCached] = retrieveElements(obj, indices)
-            elements = obj.Cache(indices);
-            areCached = obj.areCached{indices};
+            % Determine which elements are cached
+            areCached = obj.isCached(indices);
+
+            % Preallocate output array
+            elements = cell(1, length(indices));
+
+            % Filter indices for only those in cache
+            indices = indices(areCached);
+            elements(areCached) = obj.Cache(indices);
             for index = indices
                 obj.AccessLog(index) = obj.AccessCount;
             end
         end
         function storeElement(obj, index, element)
-            cacheLength = size(obj.Cache, 2);
-            if index > cacheLength
-                obj.Cache(cacheLength+1:index) = {obj.NullElement};
-            end
+            obj.expandCache(index)
 
             % Add new element to Cache
             obj.Cache{index} = element;
+            % Record that element is cached
+            obj.Cached(index) = true;
             % Record new element size
             elementInfo = whos('element');
             obj.ElementBytes(index) = elementInfo.bytes;
             % Record element access log
             obj.AccessLog(index) = obj.AccessCount;
 
-            if ~isnan(obj.MaxElements)
+            if ~isempty(obj.MaxElements)
                 [~, accessOrder] = sort(obj.AccessLog, 2, "descend");
                 idxToRemove = accessOrder(obj.MaxElements+1:end);
                 obj.removeFromCache(idxToRemove);
             end
-            if ~isnan(obj.MaxBytes)
+            if ~isempty(obj.MaxBytes)
                 [~, accessOrder] = sort(obj.AccessCount, 2, "descend");
                 totalBytes = sum(obj.AccessLog);
                 if totalBytes > obj.MaxBytes
@@ -90,6 +99,33 @@ classdef MemoryCache < handle
         end
     end
     methods (Access = private)
-
+        function expandCache(obj, index)
+            % Add trailing null elements up to index, if index is greater
+            % than cache length
+            lastCachedIndex = size(obj.Cache, 2);
+            idx = lastCachedIndex+1:max(index);
+            if ~isempty(idx)
+                obj.Cache(idx) = {[]};
+                obj.Cached(idx) = false;
+                obj.ElementBytes(idx) = 0;
+                obj.AccessLog(idx) = 0;
+            end
+        end
+        function trimCache(obj)
+            % Remove trailing null elements from cache
+            cacheLength = size(obj.Cache, 2);
+            lastCachedIndex = find(obj.Cached, 1, "last");
+            if isempty(lastCachedIndex)
+                idx = [];
+            else
+                idx = lastCachedIndex+1:cacheLength;
+            end
+            if ~isempty(idx)
+                obj.Cache(idx) = [];
+                obj.Cached(idx) = [];
+                obj.ElementBytes(idx) = [];
+                obj.AccessLog(idx) = [];
+            end
+        end
     end
 end
