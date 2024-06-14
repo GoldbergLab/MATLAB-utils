6646178ecc7313eb5a141c0e764f5d0762eed333
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-06-14 14:04:23 -0400

    Added ShrinkX and ShrinkY options to shrinkToContent

--- source/shrinkToContent.m
+++ source/shrinkToContent.m
@@ -14,11 +14,15 @@ function g = shrinkToContent(g, options)
 %           the vertical margin
 %       PositionType: Which Position property to use to shrink to - either
 %           InnerPosition, OuterPosition, or Position
+%       ShrinkX: Shrink object in the horizontal direction? Default is true
+%       ShrinkY: Shrink object in the vertical direction? Default is true
 %
 % This function shrinks a container widget, such as a figure or a uipanel,
 %   to fit its content, without affecting the size/shape/layout of the 
-%   content itself.
-% Please note that this used to be called shrinkFigureToContent.
+%   content itself. Despite the name, it can shrink or expand the parent
+%   widget.
+% Please note that this used to be called shrinkFigureToContent, which is
+%   deprecated and will be removed in the future.
 %
 % See also: tightenChildren, tileFigures
 %
@@ -32,6 +36,8 @@ arguments
     options.Margin (1, :) double = [0, 0]
     options.MarginUnits = 'pixels'
     options.PositionType (1, :) char {mustBeMember(options.PositionType, {'Position', 'InnerPosition', 'OuterPosition'})} = 'Position'
+    options.ShrinkX = true
+    options.ShrinkY = true
 end
 
 if length(options.Margin) == 1
@@ -70,9 +76,19 @@ end
 width = x1 - x0;
 height = y1 - y0;
 
-% Shrink the container
-g.Position(3) = g.Position(3) * width;
-g.Position(4) = g.Position(4) * height;
+% Shrink the container horizontally
+if options.ShrinkX
+    g.Position(3) = g.Position(3) * width;
+else
+    options.Margin(1) = 0;
+end
+
+% Shrink the container vertically
+if options.ShrinkY
+    g.Position(4) = g.Position(4) * height;
+else
+    options.Margin(2) = 0;
+end
 
 % Restore the child units
 for k = 1:length(g.Children)
