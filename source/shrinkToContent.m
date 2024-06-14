e8af0113d48bfb9e23bd49585a922af2726267fc
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-06-14 19:07:12 -0400

    Fixed shrinkToContent margin behavior
    It wasn't actually adding interior space in container before

--- source/shrinkToContent.m
+++ source/shrinkToContent.m
@@ -96,11 +96,11 @@ for k = 1:length(g.Children)
 end
 
 if any(options.Margin ~= 0)
-    position = getPositionWithUnits(g, options.MarginUnits);
-    position = position + [-options.Margin, 2*options.Margin];
-    setPositionWithUnits(g, position, options.MarginUnits);
+    sz = getPositionWithUnits(g, options.MarginUnits, 3:4);
+    sz = sz + 2*options.Margin;
+    setPositionWithUnits(g, sz, options.MarginUnits, 3:4);
     for k = 1:length(g.Children)
-        changePositionWithUnits(g.Children(k), options.Margin, options.MarginUnits, [1, 2]);
+        changePositionWithUnits(g.Children(k), options.Margin, options.MarginUnits, 1:2);
     end
 end
 
