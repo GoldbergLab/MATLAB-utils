7fb8af69c4a12027c115a9afc0c577d68e8354d2
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-08-14 12:52:48 -0400

    Add documentation to padanyarray

--- source/padanyarray.m
+++ source/padanyarray.m
@@ -1,4 +1,31 @@
 function B = padanyarray(A, padsize, padval, options)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% padanyarray: Equivalent to padarray but works for non-numeric arrays
+% usage:  [B] = padanyarray(A, padsize, padval, Name/Value, ...)
+%
+% where,
+%    A is any input array
+%    padsize is a 1xndims(A) array of dimensions sizes to pad to
+%    padval is a value to pad with, ignored if Method argument is set to
+%       anything but 'value'
+%    Name/Value arguments can include
+%       Direction: One of 'pre', 'post', or 'both'
+%       Method: One of 'replicate' or 'value'. 'circular' and 'symmetric'
+%           have not been implemented yet.
+%    B is the padded array
+%
+% The MATLAB builtin padarray can only handle numeric, logical, or
+%   categorical arrays, which seems like an unnecessary restriction. This
+%   function is meant to replicate the behavior of padarray (with almost
+%   the same syntax) but for arrays of any type, including cell arrays.
+%
+% See also: padarray, padto omnipad
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 arguments
     A
     padsize (1, :) double
