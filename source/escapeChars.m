f58275696f9faaf3163ff35a9472fc8fa23611e5
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-09-18 17:54:50 -0400

    escapeChars code cleanup

--- source/escapeChars.m
+++ source/escapeChars.m
@@ -1,6 +1,33 @@
 function escapedText = escapeChars(text, charsToEscape, escapeChar) 
-%charsToEscape = '&%$#_{}~^\';
-escapeCharsCell = {};
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% escapeChars: <short description>
+% usage:  [escapedText] = escapeChars(text, charsToEscape, escapeChar)
+%
+% where,
+%    text is the text to escape characters in
+%    charsToEscape is a 1xN char array of all the characters that need to
+%       be escaped
+%    escapeChar is the optional character to use to prefix the escape
+%       characters when escaping them with a regular expression
+%    escapedText is the altered text
+%
+% This function is meant to add escape characters to selected characters
+%   within the text.
+%
+% See also: 
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+arguments
+    text {mustBeText}
+    charsToEscape {mustBeText}
+    escapeChar {mustBeText} = '\'
+end
+
+escapeCharsCell = cell(1, length(charsToEscape));
 for k = 1:length(charsToEscape)
     escapeCharsCell{k} = [escapeChar, charsToEscape(k)];
 end
