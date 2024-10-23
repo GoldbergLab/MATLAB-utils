7e58fd7919d05a4bbb581be57b594a723209a781
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2024-10-23 13:50:28 -0400

    Add maxLines argument to readlines2

--- source/readlines2.m
+++ source/readlines2.m
@@ -1,4 +1,4 @@
-function lines = readlines2(filePath, lineSeparator)
+function lines = readlines2(filePath, lineSeparator, maxLines)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % readlines: read a text file and output the text split by lines
 % usage:  lines = readlines(filePath)
@@ -9,6 +9,8 @@ function lines = readlines2(filePath, lineSeparator)
 %    lineSeparator is an optional char array representing a line separator.
 %       The default value is the result of:
 %           sprintf('\r\n')
+%    maxLines is the maximum number of lines to return. If omitted, all 
+%       lines will be returned
 %    lines is a cell array containing char arrays, each of which represents
 %       one line of text in the file.
 %
@@ -21,11 +23,54 @@ function lines = readlines2(filePath, lineSeparator)
 % Email:   bmk27=cornell*org, brian*kardon=google*com
 % Real_email = regexprep(Email,{'=','*'},{'@','.'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+arguments
+    filePath char
+    lineSeparator = sprintf('\r\n');
+    maxLines double = Inf
+end
 
-if ~exist('lineSeparator', 'var') || isempty(lineSeparator)
+if isempty(lineSeparator)
     lineSeparator = sprintf('\r\n');
 end
 
+% Open file
 fileID = fopen(filePath);
-rawText = char(fread(fileID)');
-lines = split(rawText, lineSeparator);
+
+try
+    if isinf(maxLines)
+        % All lines requested, read whole file and split it
+        rawText = char(fread(fileID)');
+        % Split lines on separator
+        lines = split(rawText, lineSeparator);
+    else
+        % Only some lines requested, read in chunks until desired line count is achieved
+        chunkSize = 10;
+        rawText = '';
+        numLines = 0;
+        while numLines < maxLines
+            % Read next chunk
+            newText = fread(fileID, chunkSize)';
+            % Append to overall data
+            rawText = [rawText, newText]; %#ok<AGROW> 
+            % Count how many lines we've found
+            numLines = count(rawText, lineSeparator);
+            if isempty(newText)
+                % We've reached the end of the file before we got to the 
+                % desired number of lines
+                break
+            end
+            % Update chunk size based on previous line sizes, since reading larger chunks is probably faster
+            if numLines > 0
+                chunkSize = max([10, ceil(length(rawText) / numLines)]);
+            end
+        end
+        % Split lines on separator
+        lines = split(rawText, lineSeparator);
+        % Discard last line, which is extra
+        lines = lines(1:end-1);
+    end
+catch ME
+    % Try to ensure graceful close
+    fclose(fileID);
+    rethrow(ME);
+end
