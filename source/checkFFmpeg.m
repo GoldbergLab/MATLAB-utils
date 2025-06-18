eee7613270ea8072e044d042b09152bc148c00bd
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-06-18 12:22:42 -0400

    Update checkFFmpeg

--- source/checkFFmpeg.m
+++ source/checkFFmpeg.m
@@ -1,27 +1,29 @@
-function [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(checkFFmpeg, checkFFprobe, checkFFplay, issueWarning, issueError)
+function [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(options)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % checkFFmpeg: check if ffmpeg and related software is installed
 % usage: [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(
 %       checkFFmpeg, checkFFprobe, checkFFplay, issueWarning, issueError)
 %
 % where,
-%    checkFFmpeg is a logical indicating whether or not to check if 
+%    Name/Value arguments:
+%    CheckFFmpeg is a logical indicating whether or not to check if 
 %       ffmpeg is installed and available on the system
-%    checkFFprobe is a logical indicating whether or not to check if 
+%    CheckFFprobe is a logical indicating whether or not to check if 
 %       ffprobe is installed and available on the system
-%    checkFFplay is a logical indicating whether or not to check if 
+%    CheckFFplay is a logical indicating whether or not to check if 
 %       ffplay is installed and available on the system
-%    issueWarning is a logical indicating whether or not to issue a warning
+%    IssueWarning is a logical indicating whether or not to issue a warning
 %       if the software is not installed. Ignored if issueError is true
-%    issueError is a logical indicating whether or not to throw an error
+%    IssueError is a logical indicating whether or not to throw an error
 %       if the software is not installed.
 %    ffmpegExists is a logical indicating whether ffmpeg was found.
 %    ffprobeExists is a logical indicating whether ffprobe was found.
 %    ffplayExists is a logical indicating whether ffplay was found.
 %
-% <long description>
+% Check if ffmpeg, ffprobe, and ffplay are installed and available on the
+%    system
 %
-% See also: <related functions>
+% See also:
 %
 % Version: 1.0
 % Author:  Brian Kardon
@@ -29,22 +31,22 @@ function [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(checkFFmpeg, 
 % Real_email = regexprep(Email,{'=','*'},{'@','.'})
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 arguments
-    checkFFmpeg logical = true
-    checkFFprobe logical = true
-    checkFFplay logical = true
-    issueWarning logical = true
-    issueError logical = true
+    options.CheckFFmpeg logical = true
+    options.CheckFFprobe logical = true
+    options.CheckFFplay logical = true
+    options.IssueWarning logical = true
+    options.IssueError logical = true
 end
 
-if checkFFmpeg
+if options.CheckFFmpeg
     % Check if ffmpeg is available on this system. Warn user if not.
     [ffmpegStatus, ~] = system('where /q ffmpeg');
     ffmpegExists = ffmpegStatus == 0;
     if ~ffmpegExists
         msg = 'No ffmpeg found, ffprobe must be installed and available on the system path. See https://ffmpeg.org/download.html.';
-        if issueError
+        if options.IssueError
             error(msg)
-        elseif issueWarning
+        elseif options.IssueWarning
             warning(msg)
         end
     end
@@ -52,15 +54,15 @@ else
     ffmpegExists = false;
 end
 
-if checkFFprobe
+if options.CheckFFprobe
     % Check if ffprobe is available on this system. Warn user if not.
     [ffprobeStatus, ~] = system('where /q ffprobe');
     ffprobeExists = ffprobeStatus == 0;
     if ~ffprobeExists
         msg = 'No ffprobe found, ffprobe must be installed and available on the system path. See https://ffmpeg.org/download.html.';
-        if issueError
+        if options.IssueError
             error(msg)
-        elseif issueWarning
+        elseif options.IssueWarning
             warning(msg)
         end
     end
@@ -68,15 +70,15 @@ else
     ffprobeExists = false;
 end
 
-if checkFFplay
+if options.CheckFFplay
     % Check if ffplay is available on this system. Warn user if not.
     [ffplayStatus, ~] = system('where /q ffplay');
     ffplayExists = ffplayStatus == 0;
     if ~ffplayExists
         msg = 'No ffplay found, ffplay must be installed and available on the system path. See https://ffmpeg.org/download.html.';
-        if issueError
+        if options.IssueError
             error(msg)
-        elseif issueWarning
+        elseif options.IssueWarning
             warning(msg)
         end
     end
