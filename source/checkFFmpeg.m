56cf631fa82a813355a2e9f542dacf3f6a28b514
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-06-18 12:19:30 -0400

    Added checkFFmpeg

---
+++ source/checkFFmpeg.m
@@ -0,0 +1,85 @@
+function [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(checkFFmpeg, checkFFprobe, checkFFplay, issueWarning, issueError)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% checkFFmpeg: check if ffmpeg and related software is installed
+% usage: [ffmpegExists, ffprobeExists, ffplayExists] = checkFFmpeg(
+%       checkFFmpeg, checkFFprobe, checkFFplay, issueWarning, issueError)
+%
+% where,
+%    checkFFmpeg is a logical indicating whether or not to check if 
+%       ffmpeg is installed and available on the system
+%    checkFFprobe is a logical indicating whether or not to check if 
+%       ffprobe is installed and available on the system
+%    checkFFplay is a logical indicating whether or not to check if 
+%       ffplay is installed and available on the system
+%    issueWarning is a logical indicating whether or not to issue a warning
+%       if the software is not installed. Ignored if issueError is true
+%    issueError is a logical indicating whether or not to throw an error
+%       if the software is not installed.
+%    ffmpegExists is a logical indicating whether ffmpeg was found.
+%    ffprobeExists is a logical indicating whether ffprobe was found.
+%    ffplayExists is a logical indicating whether ffplay was found.
+%
+% <long description>
+%
+% See also: <related functions>
+%
+% Version: 1.0
+% Author:  Brian Kardon
+% Email:   bmk27=cornell*org, brian*kardon=google*com
+% Real_email = regexprep(Email,{'=','*'},{'@','.'})
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+arguments
+    checkFFmpeg logical = true
+    checkFFprobe logical = true
+    checkFFplay logical = true
+    issueWarning logical = true
+    issueError logical = true
+end
+
+if checkFFmpeg
+    % Check if ffmpeg is available on this system. Warn user if not.
+    [ffmpegStatus, ~] = system('where /q ffmpeg');
+    ffmpegExists = ffmpegStatus == 0;
+    if ~ffmpegExists
+        msg = 'No ffmpeg found, ffprobe must be installed and available on the system path. See https://ffmpeg.org/download.html.';
+        if issueError
+            error(msg)
+        elseif issueWarning
+            warning(msg)
+        end
+    end
+else
+    ffmpegExists = false;
+end
+
+if checkFFprobe
+    % Check if ffprobe is available on this system. Warn user if not.
+    [ffprobeStatus, ~] = system('where /q ffprobe');
+    ffprobeExists = ffprobeStatus == 0;
+    if ~ffprobeExists
+        msg = 'No ffprobe found, ffprobe must be installed and available on the system path. See https://ffmpeg.org/download.html.';
+        if issueError
+            error(msg)
+        elseif issueWarning
+            warning(msg)
+        end
+    end
+else
+    ffprobeExists = false;
+end
+
+if checkFFplay
+    % Check if ffplay is available on this system. Warn user if not.
+    [ffplayStatus, ~] = system('where /q ffplay');
+    ffplayExists = ffplayStatus == 0;
+    if ~ffplayExists
+        msg = 'No ffplay found, ffplay must be installed and available on the system path. See https://ffmpeg.org/download.html.';
+        if issueError
+            error(msg)
+        elseif issueWarning
+            warning(msg)
+        end
+    end
+else
+    ffplayExists = false;
+end
