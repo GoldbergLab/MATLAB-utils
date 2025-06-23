8d54982b16ef76d2e975a1d4d5dad6ea7ec97054
Brian Kardon <4707790+briankardon@users.noreply.github.com>
2025-06-23 12:35:30 -0400

    Reorganize code, improve debug output

--- source/batchCompressRawVideo.m
+++ source/batchCompressRawVideo.m
@@ -1,10 +1,12 @@
-function batchCompressRawVideo(videoRoot, options)
+function record = batchCompressRawVideo(videoRoot, options)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % batchCompressRawVideo: <short description>
 % usage: bbatchCompressRawVideo(videoRoot, "Name", "Value", ...)
 %
 % where,
-%    videoRoot is <description>
+%    videoRoot is the path to the folder to be searched for videos
+%    record is a Nx2 cell array containing paths or names of videos found
+%       in column 1, and paths or names of videos converted in column 2
 %    Name/Value pairs can be:
 %       VideoExtension: The video extension to look for. Default is "avi"
 %       CRF: The h264 "constant rate factor". 0 is lossless, 52 is max
@@ -19,6 +21,11 @@ function batchCompressRawVideo(videoRoot, options)
 %           actually converting any videos. Default is true.
 %       Overwrite: A logical indicating whether or not to overwrite files 
 %           that already exist. Default is false.
+%       Parallelize: A logical indicating whether or not to batch convert
+%           in parallel or not. Default is true.
+%       RecordNameOnly: A logical indicating if only names of files should
+%           be included in the output record, rather than full paths. 
+%           Default is true.
 %
 % This function is designed to batch convert videos encoded with the
 %   "rawvideo" codec to a compressed video format.
@@ -38,47 +45,96 @@ arguments
     options.CompressedVideoTag {mustBeTextScalar} = ''
     options.DryRun {logical} = true
     options.OverWrite logical = false
+    options.Parallelize logical = true
+    options.RecordNameOnly logical = true
 end
 
 % Get list of videos with that extension
 options.VideoExtension = regexprep(options.VideoExtension, '[^a-zA-Z0-9]', '');
-videos = findFiles(videoRoot, ['.*\.', options.VideoExtension], "SearchSubdirectories", options.SearchSubdirectories);
+videos = findFiles( ...
+    videoRoot, ...
+    ['.*\.', options.VideoExtension], ...
+    "SearchSubdirectories", ...
+    options.SearchSubdirectories ...
+    );
 
-% Loop over videos
-parfor k = 1:length(videos)
-    fprintf('Converting %d of %d\n', k, length(videos));
-    videoPath = videos{k};
+record = cell(length(videos), 2);
 
-    % Construct compressed filename
-    [root, name, ext] = fileparts(videoPath);
-    compressedVideoPath = fullfile(root, [name, options.CompressedVideoTag, ext]); %#ok<PFBNS>
-    try
-        if ~options.DryRun
-            % Not a dry run, compress the video
-            if strcmp(videoPath, compressedVideoPath)
-                % Original and compressed paths are the same, gotta do a dance
-                nameSwap = true;
-                finalCompressedVideoPath = compressedVideoPath;
-                [root, name, ext] = fileparts(compressedVideoPath);
-                compressedVideoPath = fullfile(root, [name, '_TEMP_COMPRESSED_', char(datetime('today')), ext]);
-            else
-                nameSwap = false;
-            end
-            compressRawVideo(videoPath, compressedVideoPath, "CheckFFmpeg", false, "CRF", options.CRF, "VerifyRaw", true, 'OverWrite', options.OverWrite);
-            if nameSwap
-                % Overwrite original
-                movefile(compressedVideoPath, finalCompressedVideoPath);
-            end
-        else
-            % Dry run, print what would have been done
-            fprintf('DRY RUN: Would have converted\n\t%s \n\t\tto \n\t%s\n', videoPath, compressedVideoPath);
+if options.Parallelize
+    % Loop over videos in parallel
+    parfor k = 1:length(videos)
+        record(k, :) = compressSingleVideo(videos, k, options);
+    end
+else
+    % Loop over videos
+    for k = 1:length(videos)
+        record(k, :) = compressSingleVideo(videos, k, options);
+    end
+end
+
+% If requested, remove path info from record
+if options.RecordNameOnly
+    for r = 1:size(record, 1)
+        for c = 1:size(record, 2)
+            [~, name, ext] = fileparts(record{r, c});
+            record{r, c} = [name, ext];
         end
-    catch ME
-        if strcmp(ME.identifier, 'MATLAB_utils:notRawVideo')
-            % Video is not encoded with rawvideo codec, skip it
-            warning('Skipping non-raw video: %s', videoPath);
+    end
+end
+
+function record = compressSingleVideo(videos, k, options)
+fprintf('Converting %d of %d\n', k, length(videos));
+videoPath = videos{k};
+record = {videoPath, ''};
+% Construct compressed filename
+[root, name, ext] = fileparts(videoPath);
+compressedVideoPath = fullfile( ...
+    root, ...
+    [name, options.CompressedVideoTag, ext] ...
+    );
+try
+    if ~options.DryRun
+        % Not a dry run, compress the video
+        if strcmp(videoPath, compressedVideoPath)
+            % Original and compressed paths are the same, gotta do a dance
+            nameSwap = true;
+            finalCompressedVideoPath = compressedVideoPath;
+            [root, name, ext] = fileparts(compressedVideoPath);
+            compressedVideoPath = fullfile( ...
+                root, ...
+                [name, '_TEMP_COMPRESSED_', char(datetime('today')), ext] ...
+                );
         else
-            rethrow(ME)
+            nameSwap = false;
         end
+        compressRawVideo( ...
+            videoPath, ...
+            compressedVideoPath, ...
+            "CheckFFmpeg", false, ...
+            "CRF", options.CRF, ...
+            "VerifyRaw", true, ...
+            'OverWrite', options.OverWrite ...
+            );
+        if nameSwap
+            % Overwrite original
+            movefile(compressedVideoPath, finalCompressedVideoPath);
+            compressedVideoPath = finalCompressedVideoPath;
+        end
+    else
+        % Dry run, print what would have been done
+        fprintf( ...
+            'DRY RUN: Would have converted\n\t%s \n\t\tto \n\t%s\n', ...
+            videoPath, ...
+            compressedVideoPath ...
+            );
+    end
+    record{2} = compressedVideoPath;
+catch ME
+    if strcmp(ME.identifier, 'MATLAB_utils:notRawVideo')
+        % Video is not encoded with rawvideo codec, skip it
+        warning('Skipping non-raw video: %s', videoPath);
+        record{2} = '*SKIPPED*';
+    else
+        rethrow(ME)
     end
 end
